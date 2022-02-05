671fcde77e7b92efda2eeef70f2591ed20ebc5fe
roopesh04 <arvapalli12345@gmail.com>
2022-02-05 10:26:28 +0530

    added sample router

--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -17,7 +17,7 @@ public class Publisher {
     private String state;
     private String zip;
 
-    @ManyToMany
+    @OneToMany
     @JoinColumn(name="publisher_id")
     private Set<Book> books=new HashSet<>();
 
